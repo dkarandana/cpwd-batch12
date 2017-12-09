5d08bd0cb2d3aea08fa8178cd897a7b6a9e57b95
dkarandana <dhananjayakarandana@gmail.com>
2017-12-09 20:48:08 +0530

    Conditional checks

---
+++ js/and-or-not-conditional-checks.js
@@ -0,0 +1,26 @@
+/* Conditional Logics */
+
+	logic1 = 2;
+	logic2 = true;
+
+	if( logic1 && logic2 ){
+		console.log("Logic 1: ", logic1 );
+		console.log("Logic 2: ", logic2 );
+	}
+
+	
+	if( logic1 || logic2 ){
+		console.log("Logic 1: ", logic1 );
+		console.log("Logic 2: ", logic2 );
+	}
+
+	console.log( !logic2 ); // will return inverse value - false
+
+	if( logic1 !== 2 ){
+		console.log( true );
+	}
+
+
+
+
+
